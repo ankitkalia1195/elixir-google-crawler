7033456f74057c6d1e005d3712be29c0b708539c
Kalia <ankitkalia.dev@gmail.com>
2021-05-27 10:40:01 +0700

    #7 Minor change in list_keywords test

--- test/google_crawler/search_test.exs
+++ test/google_crawler/search_test.exs
@@ -3,10 +3,12 @@ defmodule GoogleCrawler.SearchTest do
   alias GoogleCrawler.Search
   alias GoogleCrawler.Search.Keyword
 
-  describe "list_keywords/2" do
+  describe "list_keywords/1" do
     test "lists the keywords for given user" do
       user = insert(:user)
+      another_user = insert(:user)
       %{id: keyword_id} = insert(:keyword, name: "travel", user: user)
+      insert(:keyword, name: "travel", user: another_user)
 
       assert [%Keyword{id: ^keyword_id}] = Search.list_keywords(user)
     end
