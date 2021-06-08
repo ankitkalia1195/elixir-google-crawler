ca0ec5c39a460fbe6215b4d13bc2d04e04e41adb
Kalia <ankitkalia.dev@gmail.com>
2021-06-08 17:15:13 +0700

    #8 As a user, I can see report details by clicking on keyword.

---
+++ test/google_crawler_web/features/keyword/view_keyword_detail_test.exs
@@ -0,0 +1,26 @@
+defmodule GoogleCrawlerWeb.Upload.ViewKeywordDetailPageTest do
+  use GoogleCrawlerWeb.FeatureCase
+  alias GoogleCrawler.Search.Result
+
+  feature "views keyword detail page when keyword is processed", %{session: session} do
+    user = insert(:user)
+    result = %Result{
+      links_count: 20,
+      all_ads: [],
+      non_ads: ["www.wikipedia.com"],
+      top_ads: [],
+      html_code: ""
+    }
+
+    %{id: keyword_id}  = insert(:keyword, name: "travel", user: user, result: result)
+
+    session
+    |> login(user.email, "hello world!")
+    |> visit(Routes.keyword_path(GoogleCrawlerWeb.Endpoint, :show, keyword_id))
+    |> assert_has(Query.text("travel"))
+    |> assert_has(Query.text("Links Count"))
+    |> assert_has(Query.text("Top Ads"))
+    |> assert_has(Query.text("Total Ads"))
+    |> assert_has(css(".table"))
+  end
+end
