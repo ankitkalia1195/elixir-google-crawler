22b602398d8a7a903bfc6ec54f2df63bcd0d6a7c
Kalia <ankitkalia.dev@gmail.com>
2021-06-09 16:49:40 +0700

    #8 Add helper for getting body class

--- lib/google_crawler_web/views/layout_view.ex
+++ lib/google_crawler_web/views/layout_view.ex
@@ -1,3 +1,16 @@
 defmodule GoogleCrawlerWeb.LayoutView do
   use GoogleCrawlerWeb, :view
+
+  alias Phoenix.{Controller, Naming}
+
+  def body_class(conn) do
+    "#{module_class_name(conn)} #{Controller.action_name(conn)}"
+  end
+
+  defp module_class_name(conn) do
+    conn
+    |> Controller.controller_module()
+    |> Naming.resource_name("Controller")
+    |> String.replace("_", "-")
+  end
 end
